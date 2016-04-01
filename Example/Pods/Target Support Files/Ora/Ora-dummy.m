24873b4f43916cadd0cd6ebfd91fbede31c5f3e5
©® Chris Roff <chrisroff@CRMacBookPro.local>
2016-04-01 16:05:37 -0500

    initial

---
+++ Example/Pods/Target Support Files/Ora/Ora-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Ora : NSObject
+@end
+@implementation PodsDummy_Ora
+@end
---
+++ Example/Pods/Target Support Files/Ora/Ora-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double OraVersionNumber;
+FOUNDATION_EXPORT const unsigned char OraVersionString[];
+
